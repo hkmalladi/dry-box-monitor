e656878b97c3b16b7e804baf5d5a1c5e3bf30a2d
hkmalladi <hkmalladi@gmail.com>
2018-10-11 17:44:15 +0000

    display on lcd

--- dht22_test.py
+++ dht22_test.py
@@ -1,14 +1,35 @@
+import socket
+import fcntl
+import struct
 import Adafruit_DHT as dht
 import httplib, urllib
 import time
+from RPLCD.gpio import CharLCD
+import RPi.GPIO as GPIO
 
+lcd = CharLCD(numbering_mode=GPIO.BOARD,cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23])
 sleep = 15
 
 key = 'EW2LDPHO42P399DK'
 
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
 while True:
     h,t = dht.read_retry(dht.DHT22, 4)
-    print ('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t, h))
+    lcd.cursor_pos = (0, 0)
+    lcd.clear()
+    lcd.write_string('T={0:0.1f}C, H={1:0.1f}%'.format(t, h))
+    lcd.cursor_pos = (1, 0)
+    try:
+        lcd.write_string(get_ip_address('wlan0'))
+    except:
+        print('Failed to get IP address')
     params = urllib.urlencode({'field1': h, 'field2':t, 'key':key })
     headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
     conn = httplib.HTTPConnection("api.thingspeak.com:80")
